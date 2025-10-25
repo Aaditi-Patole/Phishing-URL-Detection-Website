4e1bc53a68e43b79ea70dd31cbebb696a039d6c2
Aaditi Patole <appatole21@gmail.com>
2025-10-25 18:13:55 +0530

    Fixed host and port for deployment

--- app.py
+++ app.py
@@ -83,4 +83,5 @@ def predict():
     return render_template('result.html', xx=round(y_pro_non_phishing, 2), url=url)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))  
+    app.run(host='0.0.0.0', port=port)
